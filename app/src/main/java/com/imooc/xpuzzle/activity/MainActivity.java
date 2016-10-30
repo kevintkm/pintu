03f18cba1c321881f77ffefe3f2303214571aa41
田克明 <keming.tian@weimob.com>
2016-10-30 15:47:53 +0800

    测试android studio Git 提交

--- app/src/main/java/com/imooc/xpuzzle/activity/MainActivity.java
+++ app/src/main/java/com/imooc/xpuzzle/activity/MainActivity.java
@@ -123,6 +123,7 @@ public class MainActivity extends Activity implements OnClickListener {
         AlertDialog.Builder builder = new AlertDialog.Builder(
                 MainActivity.this);
         builder.setTitle("选择：");
+
         builder.setItems(mCustomItems,
                 new DialogInterface.OnClickListener() {
 
